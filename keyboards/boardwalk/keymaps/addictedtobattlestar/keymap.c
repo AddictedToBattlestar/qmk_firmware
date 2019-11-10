353350273dbf67d91097d87389ab8a059e639f7e
Andy Ryan <addictedtobsg@gmail.com>
2019-11-10 17:18:02 -0500

    Adding layout for the iris, minor adj to the boardwalk

--- keyboards/boardwalk/keymaps/addictedtobattlestar/keymap.c
+++ keyboards/boardwalk/keymaps/addictedtobattlestar/keymap.c
@@ -8,12 +8,16 @@ enum layer {
 
 #define LOWER MO(1)
 
+#define A_RENAME LSFT(KC_F6)
+#define A_EXTRACT LALT(LGUI(KC_M))
+#define A_RUN LALT(LGUI(KC_R))
+
 // Mac sleep
 #define __SLEEP S(LCTL(KC_POWER))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_ortho_hhkb(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    LSFT(KC_F6), LALT(LGUI(KC_M)),   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELT, \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    A_RENAME,    A_EXTRACT,  KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_DELT, \
   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,     KC_EQL,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,     KC_RBRC,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RGB_TOG,     RGB_MOD,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
@@ -21,11 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT_ortho_hhkb(
-    RESET, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,     LALT(LGUI(KC_R)),    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
-  _______, _______, _______, _______, _______, _______, _______,     _______,             _______,   KC_UP, _______, _______, _______, KC_F12,  \
-  KC_MPLY, _______, _______, _______, _______, _______, _______,     _______,             KC_LEFT, _______, KC_RGHT, _______, _______, _______, \
-  KC_MUTE, _______, _______, _______, _______, _______, _______,     _______,             _______, KC_DOWN, _______, _______, _______, _______, \
-           KC_VOLD, KC_VOLU, _______, _______,      KC_DELT,                 KC_ENT,               KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    RESET, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,     A_RUN,      KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
+  _______, _______, _______, _______, _______, _______, RGB_SAI,     RGB_HUI,            _______,   KC_UP, _______, _______, _______, KC_F12,  \
+  _______, _______, _______, _______, _______, _______, RGB_SAD,     RGB_HUD,            KC_LEFT, _______, KC_RGHT, _______, _______, _______, \
+  KC_MUTE, _______, _______, _______, _______, _______, RGB_VAD,     RGB_VAI,            _______, KC_DOWN, _______, _______, _______, _______, \
+           KC_VOLD, KC_VOLU, _______, _______,      KC_MPLY,                 A_RUN,               KC_HOME, KC_PGDN, KC_PGUP, KC_END
   )
 };
 
