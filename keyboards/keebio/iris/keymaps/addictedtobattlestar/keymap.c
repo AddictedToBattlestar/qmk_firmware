d5f6204f3c1b6a8ece2c5de39c306588526c3f72
Andy Ryan <john.ryan@nenaner.com>
2019-11-10 12:49:03 -0500

    Adjusting the iris

--- keyboards/keebio/iris/keymaps/addictedtobattlestar/keymap.c
+++ keyboards/keebio/iris/keymaps/addictedtobattlestar/keymap.c
@@ -28,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      RGB_TOG, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,          KC_LGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LOWER,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,          KC_LGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LALT, KC_LCTL, KC_BSPC,                   KC_SPC,  KC_ENT,  RAISE
+                                    KC_LALT, KC_LCTL, KC_BSPC,                   KC_SPC,  LOWER,   RAISE
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
