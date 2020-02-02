86c91d329fb072d64d3313849f485410d7fbb519
John Ryan <john.ryan@cardinalhealth.com>
2020-02-02 17:30:54 -0500

    moving around the gui and lower keys

--- keyboards/boardwalk/keymaps/addictedtobattlestar/keymap.c
+++ keyboards/boardwalk/keymaps/addictedtobattlestar/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,     KC_EQL,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,     KC_RBRC,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RGB_TOG,     RGB_MOD,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-           LOWER,   KC_LCTL, KC_LALT, KC_LGUI,       KC_BSPC,                  KC_SPC,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+           KC_LGUI,   KC_LCTL, KC_LALT, LOWER,       KC_BSPC,                  KC_SPC,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   [_LOWER] = LAYOUT_ortho_hhkb(
