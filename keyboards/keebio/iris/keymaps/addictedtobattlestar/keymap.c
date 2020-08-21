ceb9f2a24eb3464c568a677e56feaed6bc37c4bb
John Ryan <john.ryan@cardinalhealth.com>
2020-08-21 14:36:26 -0400

    moving the directionals

--- keyboards/keebio/iris/keymaps/addictedtobattlestar/keymap.c
+++ keyboards/keebio/iris/keymaps/addictedtobattlestar/keymap.c
@@ -52,11 +52,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      RGB_M_P, RGB_M_K, RGB_M_B, RGB_M_X, _______, RGB_RMOD,                           RGB_MOD, _______, _______, _______, _______, RESET,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_HUI, RGB_HUD, _______, _______, _______, _______,                            _______, KC_PGUP, _______, _______, _______, _______,
+     RGB_HUI, RGB_HUD, _______, _______, KC_UP,   _______,                            _______, KC_PGUP, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_SAI, RGB_SAD, _______, _______, _______, _______,                            KC_LEFT, KC_NO,   KC_RGHT, _______, _______, _______,
+     RGB_SAI, RGB_SAD, _______, KC_LEFT, KC_NO,   KC_RGHT,                            KC_LEFT, KC_NO,   KC_RGHT, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,          _______, _______, KC_PGDN, _______, _______, _______, _______,
+     RGB_VAI, RGB_VAD, _______, _______, KC_DOWN, _______, _______,          _______, _______, KC_PGDN, _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, KC_MPRV,                   KC_MPLY, KC_MNXT, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -117,33 +117,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-// void encoder_update_user(uint8_t index, bool clockwise) {
-//     if (index == 0) {
-//         if (clockwise) {
-//             tap_code(KC_VOLU);
-//         } else {
-//             tap_code(KC_VOLD);
-//         }
-//     }
-//     else if (index == 1) {
-//         if (clockwise) {
-//             tap_code(KC_BRIU);
-//         } else {
-//             tap_code(KC_BRID);
-//         }
-//     }
-// }
-
 void encoder_update_user(uint8_t index, bool clockwise) {
     switch (biton32(layer_state)) {
       default:
-        tap_code16(!clockwise ? KC_VOLD : KC_VOLU);
-        break;
-      case _RAISE:
-        tap_code16(!clockwise ? KC_PGUP : KC_PGDN);
+        tap_code(clockwise ? KC_VOLU : KC_VOLD);
         break;
       case _LOWER:
-        tap_code(!clockwise ? KC_BRIU : KC_BRID);
+        tap_code(clockwise ? KC_MS_WH_DOWN : KC_MS_WH_UP);
+        break;
+      case _RAISE:
+        tap_code(clockwise ? KC_PGDN : KC_PGUP);
         break;
     }
 }
