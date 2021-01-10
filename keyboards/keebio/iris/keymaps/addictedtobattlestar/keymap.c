59f9fd35f6facd6575464c6ea80bae9b49b276b4
AddictedToBattlestar <addictedtobsg@gmail.com>
2021-01-10 15:16:28 -0500

    Trying out mod-taps for del and bkspc

--- keyboards/keebio/iris/keymaps/addictedtobattlestar/keymap.c
+++ keyboards/keebio/iris/keymaps/addictedtobattlestar/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,          KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RAISE,
         //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                          KC_LALT, KC_LCTL, KC_LGUI,                   KC_ENT,  KC_SPC,  LOWER
+                            LALT_T(KC_DEL), LCTL_T(KC_BSPC), KC_LGUI,                   KC_ENT,  KC_SPC,  LOWER
         //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
         ),
 
